99e8e908a4901e9637de53a82caa30ad3b3af699
Kutman02 <kutmank9@gmail.com>
2025-03-10 15:21:42 +0600

    Обновить @reduxjs/toolkit до версии 2.6.1 и улучшить обработку асинхронного запроса для получения фильмов

--- src/Redux/slices/moviesSlice.js
+++ src/Redux/slices/moviesSlice.js
@@ -1,21 +1,42 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
+import { addCase } from '@reduxjs/toolkit/dist/query/core/buildInitiate';
+import { films } from '../../data/films';
 const initialState = {
   films: [],
   status: null,
   errors: null,
 };
 
-export const fetchMovies = () => createAsyncThunk('movie/fetchMovies',async () => {
-     await fetch('https://67c56962c4649b9551b69643.mockapi.io/ReactCinema');
-    
-} );
-
+export const fetchMovies = () =>
+  createAsyncThunk('movie/fetchMovies', async () => {
+    try {
+      const movies = await fetch('https://67c56962c4649b9551b69643.mockapi.io/ReactCinema');
+      if (!movies.ok) {
+        throw new Error('Failed to fetch movies error.');
+      }
+      return movies.json();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  });
 const moviesSlice = createSlice({
   name: 'movie',
   initialState,
-  reducers: {},
+  reducers: {
+    test: (state, action) => {
+      console.log(state, films);
+      console.log(action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMovies.pending, (state) => {
+      state.status = 'loading';
+    });
+    addCase(fetchMovies.fullfilled, (state, actions) => {});
+    addCase(fetchMovies.rejected, (state) => {});
+  },
 });
 
 export default moviesSlice.reducer;
-//export const {  } = moviesSlice.actions;
+export const { test } = moviesSlice.actions;
--- src/components/Movies/MoviesList.jsx
+++ src/components/Movies/MoviesList.jsx
@@ -1,12 +1,16 @@
 //import { useContext } from 'react';
 //import MoviesCard from './MoviesCard';
 //import { Films } from '../../App';
+import { useDispatch } from 'react-redux';
+import { test } from '../../Redux/slices/moviesSlice';
 
 function MoviesList() {
+  const dispatch = useDispatch();
   //const films = useContext(Films);
 
   return (
     <div id="movies" className="movies anchor">
+      <button onClick={() => dispatch(test('hellooo'))}></button>
       {/*{films.map((value, index) => (
         <MoviesCard key={index} {...value} />
       ))}*/}

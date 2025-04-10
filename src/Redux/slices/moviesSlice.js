75dc6e49744e6c8b31966bd451aa507cc5daa0e6
Kutmanbek <kutmank9@gmail.com>
2025-04-10 17:01:35 +0600

    Упрощение функции fetchMovies с использованием axios и удаление устаревшего кода

--- src/Redux/slices/moviesSlice.js
+++ src/Redux/slices/moviesSlice.js
@@ -1,23 +1,32 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
+import axios from 'axios';
 const initialState = {
   films: [],
   status: null,
   errors: null,
 };
 
-export const fetchMovies = createAsyncThunk('movie/fetchMovies', async () => {
-  try {
-    const movies = await fetch('https://67c56962c4649b9551b69643.mockapi.io/ReactCinema');
-    if (!movies.ok) {
-      throw new Error('Failed to fetch movies error.');
-    }
-    return movies.json();
-  } catch (error) {
+export const fetchMovies = createAsyncThunk('movie/fetchMovies',  () => {
+  axios.get('https://67c56962c4649b9551b69643.mockapi.io/ReactCinema');
+  .then((response) => {
+    console.log(response);
+    return response;
+  })
+  .catch((error) => {
     console.error(error);
     throw error;
-  }
-});
+  })
+  //try {
+  //  const movies = await fetch('https://67c56962c4649b9551b69643.mockapi.io/ReactCinema');
+  //  //if (!movies.ok) {
+  //  //  throw new Error('Failed to fetch movies error.');
+  //  //}
+  //  return movies.json();
+  //} catch (error) {
+  //  console.error(error);
+  //  throw error;
+  //}
+})
 
 const moviesSlice = createSlice({
   name: 'movie',
